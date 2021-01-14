3a1338c50a8e538f812287140e76502c1d9e14b5
rootkit7628 <arleme.dev7@gmail.com>
2021-01-14 20:07:37 +0300

    Create script to get fb user id

---
+++ scripts/getUserId.py
@@ -0,0 +1,17 @@
+import requests, re
+from bs4 import BeautifulSoup
+
+def getUserId(username):
+    r = requests.get("https://mbasic.facebook.com/" + username)
+
+    src_code = BeautifulSoup(r.text, 'html.parser')
+
+    for balise_a in src_code.find_all('a') :
+        link = balise_a.get('href')
+        if link.startswith('/r.php?'):
+            return re.findall(r"[0-9]{15}", link)[0]
+
+
+
+
+
