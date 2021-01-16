48f7093070fad774534f136ed6b31d2834d33e80
rootkit7628 <arleme.dev7@gmail.com>
2021-01-16 22:30:49 +0300

    Fix bug with chromium browser

--- CI-Test/sendMsg-Test.py
+++ CI-Test/sendMsg-Test.py
@@ -1,10 +1,22 @@
 import sys
 sys.path.append('../scripts/')
+
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from getUserId import getUserId
 from sendMsg import sendMsg
 
-userId = getUserId('landris18')
-sendMsg(webdriver.Chrome(), userId, 'Test du jour ! C\'est Ok')
+class SendMsgTest():
+    def __init__(self):
+        self.options = Options()
+        self.options.add_argument('--headless')
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--disable-dev-shm-usage')
+        self.options.add_argument('--remote-debugging-port=9515')
+
+        self.userId = getUserId('landris18')
+        sendMsg(webdriver.Chrome(), self.userId, 'Test du jour ! C\'est Ok')
 
 
+if __name__ == "__main__":
+    SendMsgTest()
