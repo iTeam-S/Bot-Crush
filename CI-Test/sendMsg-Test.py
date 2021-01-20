e845702243e252ae4f556bc8a2729edf31843236
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-01-20 20:46:21 +0300

    Improves sendMsg scripts
    - Add  connexion function
    - Generate/use cookies for login

--- CI-Test/sendMsg-Test.py
+++ CI-Test/sendMsg-Test.py
@@ -7,18 +7,19 @@ from getUserId import getUserId
 from sendMsg import sendMsg
 
 class SendMsgTest():
-    def __init__(self):
+    def __init__(self,username):
 
         self.crm_options = Options()
-        self.crm_options.add_argument('--headless')
-        self.crm_options.add_argument('--no-sandbox')
-        self.crm_options.add_argument('--disable-dev-shm-usage')
-        self.crm_options.add_argument('--remote-debugging-port=9515')
+        if os.environ.get('PROD'):
+            self.crm_options.add_argument('--headless')
+            self.crm_options.add_argument('--no-sandbox')
+            self.crm_options.add_argument('--disable-dev-shm-usage')
+            # self.crm_options.add_argument('--remote-debugging-port=9515')
 
-        self.userId = getUserId('landris18')
+        self.userId = getUserId(username)
         print("ID => ",self.userId)
         sendMsg(webdriver.Chrome('/usr/bin/chromedriver',options=self.crm_options), self.userId, 'Test du jour ! C\'est Ok')
 
 
 if __name__ == "__main__":
-    SendMsgTest()
+    SendMsgTest('gaetan1903')
--- scripts/sendMsg.py
+++ scripts/sendMsg.py
@@ -1,53 +1,73 @@
-import os, time
+import os, time, pickle
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-BROWSER = None
+Browser = None
 
-def pageLoaded(BROWSER):
+def pageLoaded(Browser):
     '''
         Pour verifier qu'un driver a fini de charger la page...
             True dans ce cas , sinon False
     '''
-    status = BROWSER.execute_script('return document.readyState;')
+    status = Browser.execute_script('return document.readyState;')
     print(status)
     return status == 'complete'
 
+def connexion(Browser):
 
-def sendMsg(BROWSER, userID, message):
+    #Login to the fb account
+    username_ipt = Browser.find_element_by_id("m_login_email")
+    username_ipt.send_keys(os.environ.get("ITEAMS_LOGIN"))
+    print("Variable env => ",os.environ.get("ITEAMS_LOGIN"))
+
+    password_ipt = Browser.find_element_by_name("pass")
+    password_ipt.send_keys(os.environ.get("ITEAMS_PASS"))
+    print("Variable env => ",os.environ.get("ITEAMS_PASS"))
+
+    Browser.find_element_by_name("login").click()
+
+    while not pageLoaded(Browser): time.sleep(0.5)
+    
+    with open("cookies.pkl","wb") as fcookies:
+        pickle.dump(Browser.get_cookies() , fcookies)
+
+
+def sendMsg(Browser, userID, message):
     '''
         Fonction pour envoyer un message specifique à utilisateur Facebook
             Elle admet trois parametres, le driver, l'userID (ex: 100000144) et le message en texte.
     '''
-    #Open the BROWSER and go to login page
-    BROWSER.get('http://mbasic.facebook.com/')
+    Browser.get('https://mbasic.facebook.com/')
+    while not pageLoaded(Browser): time.sleep(0.5)
 
-    #Login to the fb account
-    username_ipt = BROWSER.find_element_by_id("m_login_email")
-    username_ipt.send_keys(os.environ.get("ITEAMS_LOGIN"))
-    print("Variable env => ",os.environ.get("ITEAMS_LOGIN"))
+    if os.path.isfile('cookies.pkl'):
+        with open("cookies.pkl", "rb") as fcookies:
+            cookies = pickle.load(fcookies)
+            for cookie in cookies:
+                Browser.add_cookie(cookie)
 
-    password_ipt = BROWSER.find_element_by_name("pass")
-    password_ipt.send_keys(os.environ.get("ITEAMS_PASS"))
-    print("Variable env => ",os.environ.get("ITEAMS_PASS"))
-
-    BROWSER.find_element_by_name("login").click()
-    while not pageLoaded(BROWSER): time.sleep(0.5)
-  
+    else: connexion(Browser)
     #Redirect to the message page of the user
-    BROWSER.get('http://mbasic.facebook.com/messages/thread/'+ userID)
-    while not pageLoaded(BROWSER): time.sleep(0.5)
+    Browser.get('https://mbasic.facebook.com/messages/thread/'+ userID)
+    while not pageLoaded(Browser): time.sleep(0.5)
+
+    if 'login.php' in Browser.current_url: 
+        connexion(Browser)
+        Browser.get('https://mbasic.facebook.com/messages/thread/'+ userID)
+        while not pageLoaded(Browser): time.sleep(0.5)
 
     # If the user is not a friend
     try:
-        message_ipt = WebDriverWait(BROWSER, 10).until(EC.presence_of_element_located((By.NAME, "body")))
+        message_ipt = WebDriverWait(Browser, 10).until(EC.presence_of_element_located((By.NAME, "body")))
     #If the user is a friend
     except:
-        message_ipt = WebDriverWait(BROWSER, 10).until(EC.presence_of_element_located((By.ID, "composerInput")))
-    else:
-        print("Can't find input")
+        message_ipt = WebDriverWait(Browser, 10).until(EC.presence_of_element_located((By.ID, "composerInput")))
+    
     message_ipt.send_keys(message)
-    BROWSER.find_element_by_name("Send").click()
+    try:
+        Browser.find_element_by_name("Send").click()
+    except:
+        Browser.find_element_by_name("send").click()
