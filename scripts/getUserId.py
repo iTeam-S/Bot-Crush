05d3269b63daf3dd674ee7399f6bee3fda2e1464
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-01-20 19:40:19 +0300

    GET userId on lock account
    - need to login using cookies

--- scripts/getUserId.py
+++ scripts/getUserId.py
@@ -7,7 +7,15 @@ def getUserId(username):
         A partir d'un username du profil facebook, 
             retourne l'userID sinon None si Not found
     '''
-    r = requests.get("https://mbasic.facebook.com/" + username)
+
+    # Les données du cookies du navigateur
+    auth_cookies = {
+        'c_user': '',
+        'xs': '' 
+    }
+
+    s = requests.Session()
+    r = s.get("https://mbasic.facebook.com/" + username, cookies=auth_cookies)
     
     if r.status_code == 404:
         return
@@ -16,11 +24,5 @@ def getUserId(username):
 
     for balise_a in src_code.find_all('a') :
         link = balise_a.get('href')
-        if link.startswith('/r.php?'):
+        if link.startswith('/r.php?') or 'profile_id' in link:
             return re.findall(r"[0-9]{15}", link)[0]
-
-
-
-
-
-
--- scripts/sendMsg.py
+++ scripts/sendMsg.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-from getUserId import getUserId
 BROWSER = None
 
 def pageLoaded(BROWSER):
@@ -23,39 +22,23 @@ def sendMsg(BROWSER, userID, message):
         Fonction pour envoyer un message specifique à utilisateur Facebook
             Elle admet trois parametres, le driver, l'userID (ex: 100000144) et le message en texte.
     '''
-    x = os.environ.get("ITEAMS_LOGIN")
-    print("Variable env => ", x)
-    print("Variable env => ",os.environ.get("ITEAMS_PASS"))
-    
     #Open the BROWSER and go to login page
     BROWSER.get('http://mbasic.facebook.com/')
 
     #Login to the fb account
     username_ipt = BROWSER.find_element_by_id("m_login_email")
     username_ipt.send_keys(os.environ.get("ITEAMS_LOGIN"))
-   
+    print("Variable env => ",os.environ.get("ITEAMS_LOGIN"))
+
     password_ipt = BROWSER.find_element_by_name("pass")
     password_ipt.send_keys(os.environ.get("ITEAMS_PASS"))
-    
-    body = BROWSER.find_element_by_tag_name("body")
-    print(body.get_attribute('outerHTML'))
-    body.screenshot("./log0.png")
+    print("Variable env => ",os.environ.get("ITEAMS_PASS"))
 
     BROWSER.find_element_by_name("login").click()
-    while not pageLoaded(BROWSER):
-        time.sleep(0.5)
-    body = BROWSER.find_element_by_tag_name("body")
-    print(body.get_attribute('outerHTML'))
-    body.screenshot("./log1.png")
-
+    while not pageLoaded(BROWSER): time.sleep(0.5)
+  
     #Redirect to the message page of the user
     BROWSER.get('http://mbasic.facebook.com/messages/thread/'+ userID)
-    body = BROWSER.find_element_by_tag_name("body")
-    print(body.get_attribute('outerHTML'))
-    body.screenshot("./log2.png")
-
-    return
-    
     while not pageLoaded(BROWSER): time.sleep(0.5)
 
     # If the user is not a friend
@@ -68,16 +51,3 @@ def sendMsg(BROWSER, userID, message):
         print("Can't find input")
     message_ipt.send_keys(message)
     BROWSER.find_element_by_name("Send").click()
-
-
-'''
-    Pour effacer l'historique de message entre le Bot et ces utilisateurs.
-'''
-
-def delete_messages( BROWSER,username):
-    page_id="100142865413064"
-    user_id=getUserId(username)
-    links="https://mbasic.facebook.com/messages/read/?tid=cid.c."+user_id+"%3A"+page_id
-    BROWSER.get(links)
-    BROWSER.find_element_by_name("delete").click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.LINK_TEXT, "Supprimer"))).click()
